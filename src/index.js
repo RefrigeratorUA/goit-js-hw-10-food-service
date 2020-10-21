ca938541f38c7b311a8a2a134d2ca2b4482000de
Dmytro Shalenyi <shalenyi.dm@gmail.com>
2020-10-21 21:41:50 +0300

    ver-1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-import './styles.css';
+import './styles.scss';
+import foodsData from './menu.json';
+import foodCardsTpl from './templates/food-cards.hbs';
+
+const bodyRef = document.querySelector('body');
+const MenuListRef = document.querySelector('.js-menu');
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+    STORAGE_KEY: 'theme',
+    CHECKBOX_STATUS: false,
+};
+
+initTheme(Theme.STORAGE_KEY);
+
+checkboxRef.addEventListener('change', changeTheme);
+
+MenuListRef.innerHTML = foodCardsTpl(foodsData);
+
+function initTheme(key) {
+    const keyinLocalStorage = localStorage.getItem(key);
+    if (keyinLocalStorage && keyinLocalStorage === Theme.DARK) {
+        checkboxRef.checked = true;
+        setDarkTheme();
+    }
+}
+
+function changeTheme() {
+    if (Theme.CHECKBOX_STATUS) setLightTheme();
+    else setDarkTheme();
+}
+
+function setLightTheme() {
+    localStorage.setItem(Theme.STORAGE_KEY, Theme.LIGHT);
+    Theme.CHECKBOX_STATUS = false;
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+}
+
+function setDarkTheme() {
+    localStorage.setItem(Theme.STORAGE_KEY, Theme.DARK);
+    Theme.CHECKBOX_STATUS = true;
+    bodyRef.classList.remove(Theme.LIGHT);
+    bodyRef.classList.add(Theme.DARK);
+}
--- webpack/configs/development.js
+++ webpack/configs/development.js
@@ -28,7 +28,7 @@ module.exports = env => ({
     publicPath: '',
     historyApiFallback: true,
     compress: true,
-    port: 4040,
+    port: 7777,
     noInfo: true,
     quiet: true,
     clientLogLevel: 'warning',
